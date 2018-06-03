4d3d9e616e6dbc59f696eb277f654207b115d1a4
Czxck001 <Czxck001@outlook.com>
2018-06-03 23:46:01 +0800

    add comments in collision checking code

--- utils/collide.js
+++ utils/collide.js
@@ -5,6 +5,7 @@ exports.hitTheLeft = takeBoxified(hitTheLeft);
 exports.hitTheRight = takeBoxified(hitTheRight);
 
 function boxify(sprite) {
+  // Boxify the sprite so that we can handily detect box collision
   let box = {};
 
   box.l = sprite.x;
@@ -16,6 +17,8 @@ function boxify(sprite) {
 }
 
 function takeBoxified(func) {
+  // Wrap a function taking sprite-like (x-width, y-height) object
+  // into a function taking boxes (l, r, u, d).
   return (...params) => {
     let boxifieds = params.map(boxify);
     return func(...boxifieds);
@@ -31,14 +34,21 @@ function overlapY(a, b) {
 }
 
 function hitTheTop(a, at, b) {
-  return (a.d <= b.u && at.d > b.u
-          || a.d < b.u && at.d == b.u)
-         && (overlapX(a, b) || overlapX(at, b));
+  // Detection of a hit by a moving object (a) to a static object (b)
+  // in one direction (top for this function).
+  // a is the current status of the moving object; at is the predicted (next)
+  // status of the moving object.
+  return (
+    // a currently above or on b, while at the next moment a is below b
+    a.d <= b.u && at.d > b.u
+    // Or, a currently above b, while at the next moment a is on b
+    || a.d < b.u && at.d == b.u
+    // And, a or at overlaps b in another dimension (horizontally).
+  ) && (overlapX(a, b) || overlapX(at, b));
 }
 
 function onTopOf(a, b) {
-  return a.d == b.u
-         && overlapX(a, b);
+  return a.d == b.u && overlapX(a, b);
 }
 
 function hitTheBottom(a, at, b) {
